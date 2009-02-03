00fc1313ce195886771c9a26aa7a3396952f2d89
Lourens Naude <lourens@methodmissing.com>
2009-02-03 14:59:06 +0000

    Sanitize for *, / and . in format strings

--- lib/scrooge/tracker/resource.rb
+++ lib/scrooge/tracker/resource.rb
@@ -39,7 +39,7 @@ module Scrooge
       # Generates a signature / lookup key.
       #
       def signature
-        @signature ||= "#{controller.to_s}_#{action.to_s}_#{method.to_s}_#{format.to_s}".gsub( '/', '_' )
+        @signature ||= "#{controller.to_s}_#{action.to_s}_#{method.to_s}_#{format.to_s}".gsub( /\/|\*|\./, '_s' )
       end
       
       # Only track GET requests
