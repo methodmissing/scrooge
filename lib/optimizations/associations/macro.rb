c6c160acdc9325bad7adc5df60a4f19fc1473e36
Lourens Naude <lourens@methodmissing.com>
2009-03-16 18:09:59 +0000

    Mark associations referenced on scrooged records

--- lib/optimizations/associations/macro.rb
+++ lib/optimizations/associations/macro.rb
@@ -25,12 +25,28 @@ module Scrooge
       end
       
       module SingletonMethods
-        
-        
-        
       end
       
       module InstanceMethods
+
+        def association_instance_get(name)
+          association = instance_variable_get("@#{name}")
+          if association.respond_to?(:loaded?)
+            scrooge_seen_association!( name )
+            association
+          end
+        end
+
+        def association_instance_set(name, association)
+          scrooge_seen_association!( name )
+          instance_variable_set("@#{name}", association)
+        end
+        
+        def scrooge_seen_association!( association )
+          if scrooged?
+            self.class.scrooge_callsite( @attributes.callsite_signature ).association!( association ) 
+          end
+        end
         
       end
       
